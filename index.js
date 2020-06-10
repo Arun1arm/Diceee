196b604b724f90f9d0e0b5e503b69480088778ed
Arun kumar Mohanty <32359237+Arun1arm@users.noreply.github.com>
2020-06-10 15:09:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+//alert("working")
+var randomNumber1 = Math.floor(Math.random() * 6) + 1 ;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1 ;
+
+var decisionHeading = document.getElementsByTagName("h1")[0].innerText
+
+if(randomNumber1 > randomNumber2){
+	//alert("1 wins");
+	document.getElementsByTagName("h1")[0].innerText = "Player 1 wins!!!"
+
+}
+else if(randomNumber2 > randomNumber1){
+	//alert("2 wins");
+	document.getElementsByTagName("h1")[0].innerText = "Player 2 wins!!!"
+}
+else{
+	//alert("Tie!!!");
+	document.getElementsByTagName("h1")[0].innerText = "It's a tie!!!"
+}
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource1 = "C:/Users/arunm/Documents/WebDev_Projects/Dicee_Challenge/images/" + randomDiceImage1;
+var randomImageSource2 = "C:/Users/arunm/Documents/WebDev_Projects/Dicee_Challenge/images/" + randomDiceImage2;
+
+
+var diceImage1 = document.querySelectorAll("img")[0]
+var diceImage2 = document.querySelectorAll("img")[1]
+
+diceImage1.setAttribute("src", randomImageSource1);
+diceImage2.setAttribute("src", randomImageSource2);
+
+
